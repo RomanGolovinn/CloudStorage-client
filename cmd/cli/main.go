2b5ba56e8b18bfe6f1de91f83ed44e6154f65b72
RomanGolovinn <golovinroman.ev@gmail.com>
2025-10-27 23:18:41 +0300

    cli start coding, doesn work version

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"fmt"
+	"os"
 	"CloudStorage-client/internal/client/commands"
 )
 
 func main(){
-	commands.Ecexcute()
+	if err := commands.RootCmd.Execute(); err != nil{
+		fmt.Println(err)
+		os.Exit(1)
+	}
 }

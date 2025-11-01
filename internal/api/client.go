0f8112f8efac7badc68711ba927b5905e08fb36c
RomanGolovinn <golovinroman.ev@gmail.com>
2025-11-01 18:45:08 +0300

    doRequests

--- internal/api/client.go
+++ internal/api/client.go
@@ -1,8 +1,13 @@
 package api
 
 import (
-	"CloudStorage-client/pkg/common"
+	"bytes"
+	"fmt"
+	"io"
+	"io/ioutil"
 	"net/http"
+	"strconv"
+	"time"
 )
 
 type Client struct {
@@ -11,11 +16,32 @@ type Client struct {
 	baseURL    string
 }
 
-func (c *Client) doRequest() {
-	// doRequest выполняет HTTP-запрос,
-	// добавляет заголовок авторизации и декодирует ответ.
+func NewClient(baseURL, token string) *Client {
+	return &Client{
+		Token: token,
+		httpClient: http.Client{
+			Timeout: time.Minute,
+		},
+		baseURL: baseURL,
+	}
 }
 
-func (c *Client) RequestFileList() []common.FileInfo {
-	// запрос на список всех файлов
+func (c *Client) doRequest(method string, url string, body interface{}) (*http.Response, error) {
+	req, err := http.NewRequest(method, url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if body != nil {
+		req.Body = ioutil.NopCloser(bytes.NewBufferString(fmt.Sprintf("{%s: %s}", "body", body)))
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Content-Length", strconv.Itoa(len(req.Body.(*io.Reader).Size())))
+	}
+
+	resp, err := c.httpClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+
+	return resp, nil
 }
